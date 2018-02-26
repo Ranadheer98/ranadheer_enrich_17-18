dee89f21fc7435378ab9967008d5f7bf80ac9b48
Ranadheer98 <35226275+Ranadheer98@users.noreply.github.com>
2018-02-26 18:58:50 +0530

    Create leftrotate.c

---
+++ leftrotate.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h> 
+int main() 
+{
+	int i,n;
+	char a[100];
+	scanf("%s", a);
+	scanf("%d", &n);
+	for(i=n; i<strlen(a); i++)
+	{
+	    printf("%c", a[i]);
+	}
+	for(i=0; i<n; i++)
+	{
+	    printf("%c", a[i]);
+	}
+	return 0;
+}
